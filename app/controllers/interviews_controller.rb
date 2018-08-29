52948945323efcf17ab191e4c775aed06718589f
hy4yh <hy4yh@users.noreply.github.com>
2018-08-29 13:58:30 +0900

    get_current_user_recordを削除しcurrent_userでレコード取得に変更

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,18 +1,15 @@
 class InterviewsController < ApplicationController
   def index
-    user = get_current_user_record
-    @interviews = user.interviews.order(:interview_datetime)
+    @interviews = current_user.interviews.order(:interview_datetime)
   end
 
   def new
-    user = get_current_user_record
-    @interview = user.interviews.new
+    @interview = current_user.interviews.new
   end
 
   def create
     if current_user?
-      user = get_current_user_record
-      @interview = user.interviews.new(interview_param)
+      @interview = current_user.interviews.new(interview_param)
       if @interview.save
         flash.now[:notice] = "面接を作成しました。"
       else
@@ -52,9 +49,5 @@ class InterviewsController < ApplicationController
   private
     def interview_param
       params.require(:interview).permit(:interview_datetime)
-    end
-
-    def get_current_user_record
-      User.find(current_user.id)
     end
 end

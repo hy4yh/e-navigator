4c816aaac31baf3190452c2418d1182225f8c1ac
hy4yh <hy4yh@users.noreply.github.com>
2018-09-20 21:33:17 +0900

    面接決定メール送信の実装(Mailer)

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -5,4 +5,12 @@ class InterviewMailer < ApplicationMailer
     mail to: interviewer_email,
          subject: '面接希望日が決まりました。'
   end
+
+  def decide(interviewee, interviewer)
+    @interviewee = interviewee
+    @interviewee_interviews = interviewee.interviews
+    @interviewer = interviewer
+    mail to: [@interviewee.email, @interviewer.email],
+         subject: '面接日時が確定しました。'
+  end
 end

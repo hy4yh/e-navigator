cb051e3f3d237cdb443873b3271cf37054b1c4c4
hy4yh <hy4yh@users.noreply.github.com>
2018-09-23 21:23:58 +0900

    profilesテーブルのuser_idカラムにunique制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,14 @@ ActiveRecord::Schema.define(version: 20180827090732) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "name"
     t.date "birthday"
     t.integer "gender", default: 0, null: false
     t.string "school_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +48,5 @@ ActiveRecord::Schema.define(version: 20180827090732) do
   end
 
   add_foreign_key "interviews", "users"
+  add_foreign_key "profiles", "users"
 end
